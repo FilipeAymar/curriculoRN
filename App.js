ff29ac5d3ac9fb49b993d36a7e75ee2597c8a32c
Filipe Aymar <130711610+FilipeAymar@users.noreply.github.com>
2024-05-23 21:15:34 -0300

    criação de curriculo no expo app

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import Homepage from './Pages/Pagehome';
 
-export default function App() {
+// Componentes de página};
+
+const App = () => {
+  const [menuAberto, setMenuAberto] = useState(false);
+  const [paginaAtual, setPaginaAtual] = useState('Página Inicial');
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
+  const navegarPara = (pagina) => {
+    setPaginaAtual(pagina);
+    setMenuAberto(false);
+  };
+
+  // Renderização condicional da página atual
+  const renderizarPagina = () => {
+    switch (paginaAtual) {
+      case 'Página Inicial':
+        return <Homepage/>;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Cabeçalho */}
+      <View style={styles.header}>
+        <Text style={styles.titulo}>Meu Curriculo</Text> 
+      </View>
+
+      {/* Menu */}
+      {menuAberto && (
+        <View style={styles.menu}>
+          <TouchableOpacity style={styles.menuItem} onPress={() => navegarPara('Pagehome')}>
+            <Text style={styles.menuItemText}>Página Inicial</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {/* Conteúdo */}
+      <View style={styles.content}>{renderizarPagina()}</View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 24,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  menuIcon: {
+    padding: 10,
+  },
+  menu: {
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+  },
+  menuItem: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  menuItemText: {
+    fontSize: 16,
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pagina: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textoPagina: {
+    fontSize: 18,
+  },
+  negrito:{
+    fontWeight:'semibold',
   },
 });
+
+export default App;
